246adccc1b638373705b84b00441aaf7ca3bd2a3
Denys Mishunov <denys.mishunov@gmail.com>
2015-10-08 14:29:27 +0200

    Angular-driven scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ioweyouApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ioweyouApp
+ */
+angular.module('ioweyouApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ioweyouApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ioweyouApp
+ */
+angular.module('ioweyouApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
